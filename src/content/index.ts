fc4416799e29570759d73dfde078e131b658634a
Siddhant Singh <siddhantsingh@Siddhants-MacBook-Pro.local>
2026-05-31 23:14:25 +0530

    initial: project setup, manifest.json and content script

---
+++ src/content/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener('mouseup',() => {
+    const selectedText = window.getSelection()?.toString().trim()
+if (selectedText) {
+    chrome.runtime.sendMessage({type: "TEXT_SELECTED", text: selectedText })
+}
+})
