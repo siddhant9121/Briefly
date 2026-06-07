4540569721f7280fbfc9df9285b13946f7f46208
Siddhant <siddhant@Siddhants-MacBook-Pro.local>
2026-06-07 16:23:50 +0530

    day 3: popup UI complete

---
+++ src/popup/index.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+export default function Popup() {
+    const [selectedText, setSelectedText] = useState ('')
+    const [ mode, setMode]= useState ('Student')
+ useEffect(() => {
+    chrome.storage.local.get('selectedText', (result) => {
+        if (result.selectedText) {
+            setSelectedText(result.selectedText)
+        }
+    })
+}, [])
+
+return(
+    <div> 
+        <h1>Briefly</h1>
+        <div>
+            <button onClick={() => setMode('student')} >Student</button>
+            <button onClick={() => setMode('Research')} >Research</button>
+            <button onClick={() => setMode('Executive')} >Executive</button>
+        </div>
+        <p> {selectedText}</p>
+    </div>
